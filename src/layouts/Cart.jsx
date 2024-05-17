f2a92b4f1daa850c4f4e838fdde28ab1b81804cd
AngelValentino <138892399+AngelValentino@users.noreply.github.com>
2024-05-18 01:20:00 +0200

    Added cart footer.

--- src/layouts/Cart.jsx
+++ src/layouts/Cart.jsx
@@ -16,9 +16,9 @@ const CartModal = ({closeBtn}) => {
   return ( 
     <>
       <div className="items-container">
-      {cartItems.length === 0 
-        ? <p>Your cart is currently empty.</p> 
-        : cartItems.slice().reverse().map(({id, variantId, imgUrl, title, size, price, quantity }) => (
+        {cartItems.length === 0 
+          ? <p>Your cart is currently empty.</p> 
+          : cartItems.slice().reverse().map(({id, variantId, imgUrl, title, size, price, quantity }) => (
           <div className="cart-item" key={variantId}>
             <Link className="cart-item__product-img-link" to={`/jackets/${id}`} onClick={closeModal}>
               <img className="cart-item__product-img" src={imgUrl} alt={title} />
@@ -79,7 +79,21 @@ const CartModal = ({closeBtn}) => {
           </>
         }
       </div>
-      <div className="cart-total">total: {cartItems.reduce((acc, currItem) => acc + currItem.price * currItem.quantity, 0)}</div>
+      <div className="cart-footer">
+        <div className="cart-footer__dicount-code-container">
+          <div>
+            <label className="visually-hidden" htmlFor="cart-footer__discount-code-input">Discount Code</label>
+            <input placeholder="Discount code" id="cart-footer__discount-code-input" className="cart-footer__discount-code-input" type="text" />
+          </div>
+          <button className="cart-footer__dicount-code-btn">Apply</button>
+        </div>
+        <div className="cart-footer__total-container">
+          <h2 className="cart-total__title">SUBTOTAL</h2>
+          <h3 className="cart-total__price">{formatCurrency(cartItems.reduce((acc, currItem) => acc + currItem.price * currItem.quantity, 0))}</h3>
+        </div>
+        <small className="cart-footer__small-text">Shipping, taxes, and discount codes calculated at checkout.</small>
+        <button className="cart-footer__checkout-btn">Check out</button>
+      </div>
     </>
   );
 }
