192b57bfe359493df2332632ceac0db0ec0062f6
AngelValentino <138892399+AngelValentino@users.noreply.github.com>
2024-05-18 13:38:05 +0200

    Added hover animations, improved cart components.

--- src/components/CartItemQty.jsx
+++ src/components/CartItemQty.jsx
@@ -1,6 +1,6 @@
 import { useShoppingCart } from "../context/ShoppingCartContext";
 
-const CartItemQty = ({ quantity }) => {
+const CartItemQty = ({ quantity, variantId }) => {
   const { 
     increaseCartQuantity, 
     decreaseCartQuantity, 
@@ -27,12 +27,12 @@ const CartItemQty = ({ quantity }) => {
       />
       <button className="cart-item__remove-qty-btn" onClick={() => decreaseCartQuantity(variantId)}>
         <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-          <path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"/>
+          <path fill="currentColor" d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"/>
         </svg>
       </button>
       <button className="cart-item__add-qty-btn" onClick={() => increaseCartQuantity(variantId)}>
         <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-          <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/>
+          <path fill="currentColor" d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/>
         </svg>
       </button>
     </div>
