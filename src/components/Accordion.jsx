f78e56961ae7ddf659dc116c42fdaf0fbceebabf
AngelValentino <138892399+AngelValentino@users.noreply.github.com>
2024-06-14 13:49:31 +0200

    Improved Accordion component acessibility. Added inert attibute in case there are focusable elements inside the parent element and aria-hidden doesn't work properly. This way, the content of the accordion section is always hidden if it's not visible.

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -11,7 +11,7 @@ const Accordion = ({ description, factsData, keepOthersOpen, ulClass, btnClass }
       // Returns toggled proptery for all facts, first is defaulted to true if isFaqs is false
       setAccordionItems([ 
         ...factsData.map((fact, i) => (
-          { ...fact, toggled: isFaqs ? false : i === 0}
+          { ...fact, toggled: isFaqs ? false : i === 0 }
         )) 
       ]);
     }
@@ -21,8 +21,8 @@ const Accordion = ({ description, factsData, keepOthersOpen, ulClass, btnClass }
 
   // Opens or closes the current item and checks if it has to keep the others open or not
   function toggle(clickedItem) {
-    setAccordionItems([ 
-      ...accordionItems.map(fact => (
+    setAccordionItems((prevItems) => [ 
+      ...prevItems.map(fact => (
         { ...fact, toggled: checkToggled(clickedItem, fact)}
       )) 
     ]);
@@ -47,16 +47,17 @@ const Accordion = ({ description, factsData, keepOthersOpen, ulClass, btnClass }
             {isFaqs ? <h3 className="accordion__faqs-title">{fact.title}</h3> : <h2 className="accordion__product-details-title">{fact.title.toUpperCase()}</h2>}
             {isFaqs 
               ?
-                <svg className={fact.toggled ? 'accordion__plus-icon accordion__plus-icon--open' : 'accordion__plus-icon'} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <svg aria-hidden="true" focusable="false" role="img" className={fact.toggled ? 'accordion__plus-icon accordion__plus-icon--open' : 'accordion__plus-icon'} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                   <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4v16m8-8H4" />
                 </svg>
               :
-                <svg className={fact.toggled ? 'accordion__chevron accordion__chevron--open' : 'accordion__chevron'} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <svg aria-hidden="true" focusable="false" role="img" className={fact.toggled ? 'accordion__chevron accordion__chevron--open' : 'accordion__chevron'} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                   <path fill="currentColor" d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
                 </svg>
             }
           </button>
           <div 
+            inert={!fact.toggled ? 'true' : null}
             role="region" 
             id={`accordion__section-${fact.title}`} 
             aria-hidden={!fact.toggled} 
