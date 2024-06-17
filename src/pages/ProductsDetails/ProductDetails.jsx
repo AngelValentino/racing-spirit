8fdc2e1cbd640d4d1d0b80a12435694dbb1df28f
AngelValentino <138892399+AngelValentino@users.noreply.github.com>
2024-06-17 17:36:53 +0200

    Added key to ProductInfo to force rerender. When the useFetch hook fetched data from cache, it didn't force a rerender. A key attribute has been added to ProductInfo to correct this behavior.

--- src/pages/ProductsDetails/ProductDetails.jsx
+++ src/pages/ProductsDetails/ProductDetails.jsx
@@ -7,7 +7,6 @@ import Products from '../../components/Products';
 import ProductCarousel from '../../components/ProductCarousel';
 import BouncingBallsLoader from '../../loaders/BouncingBallsLoader';
 
-
 const ProductDetails = () => {
   const { id } = useParams();
   const { data: jacket, loading, error } = useFetch('https://my-json-server.typicode.com/AngelValentino/racing-spirit-test-api/products/' + id);
@@ -18,7 +17,7 @@ const ProductDetails = () => {
       <section className="product-details-wrapper">
         { loading && <div className="product-details__loader-container"><BouncingBallsLoader /></div> }
         { error && <ErrorMessage error={error} /> }
-        { (!loading && !error) && <ProductInfo jacket={jacket} /> } 
+        { (!loading && !error) && <ProductInfo key={id} jacket={jacket} /> } 
       </section>
       <section className="product-details-recommended">
         <h2 className="product-details-recommended__title">You may also like</h2>
